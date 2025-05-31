cf2b2360ab6297868098d40430dbb94db7c34a80
StarCobaltAnimator2021 <147932170+StarCobaltAnimator2021@users.noreply.github.com>
2025-05-31 15:45:29 +0700

    doodles 1

--- Start.bat
+++ Start.bat
@@ -1,6 +1,6 @@
 @echo off
 echo:
 echo GAWN2600 Pro
-echo Evaluation copy. Build 26058.ge_release.250517-1831
+echo Evaluation copy. Build 26080.ge_release.250531-0024
 echo:
 npm start
---
+++ server/pages/checkver.bat
@@ -0,0 +1,17 @@
+@Echo off
+title Spartan Flash Animate Version Checker
+echo GAWN2600 Pro
+echo Version 25H2 (Build 26080.ge_release.250531-0024, 26080.1100)
+echo Full buildtag: 26080.1100.ge_release_svc_prod1.250531-1007
+echo:
+echo New Features and Changes
+echo:
+echo The Theme has been changed to reflect the upcoming Summer, which this Summer 2025 update were based on.
+echo:
+pause
+cls
+echo Bug Fixes.
+echo:
+echo No bugfixes were included in this build.
+echo:
+pause
