aeb3a5a624fd25f9cdf75c105075cbe35a0d65db
StarCobaltAnimator2021 <147932170+StarCobaltAnimator2021@users.noreply.github.com>
2025-06-16 04:08:35 +0700

    2nd Servicing pipeline push for beta test

---
+++ Start.bat
@@ -0,0 +1,13 @@
+@echo off
+echo:
+echo GAWN2600 Pro
+echo:
+echo Spartan Flash Animate Will now start
+echo:
+echo Closing this Window Will also close the Software but not the CMD window, Please remember to ALWAYS Close 
+echo it properly by saving the videos first. Then Go back to the home page. and use "close node.bat" To close
+echo the CMD Window. Anyways Remember to smile always while Making Videos :) and Have Fun Making Videos...
+echo:
+echo Build 26100.ge_release.250613-1521
+echo:
+npm start
