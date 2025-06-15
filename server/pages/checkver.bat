f3385f0262141833da46e885abf3edbbbb8c5d9b
StarCobaltAnimator2021 <147932170+StarCobaltAnimator2021@users.noreply.github.com>
2025-06-16 04:09:12 +0700

    period

---
+++ server/pages/checkver.bat
@@ -0,0 +1,17 @@
+@Echo off
+title Spartan Flash Animate Version Checker
+echo GAWN2600 Pro
+echo Version 25H2 (Build 26100.ge_release.250613-1521, 26100.560)
+echo Full buildtag: 26100.560.ge_release_svc_prod2.250616-0400
+echo:
+echo New Features and Changes
+echo:
+echo No new features were included onto this build.
+echo:
+pause
+cls
+echo Bug Fixes.
+echo:
+echo No bugfixes were included in this build.
+echo:
+pause
