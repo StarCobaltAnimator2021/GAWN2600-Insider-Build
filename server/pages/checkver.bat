dcae65da1ec2336bb26edb509c1eee821127ce60
StarCobaltAnimator2021 <147932170+StarCobaltAnimator2021@users.noreply.github.com>
2025-06-16 03:35:52 +0700

    Manual file move due to merge failed.

--- server/pages/checkver.bat
+++ server/pages/checkver.bat
@@ -1,12 +1,12 @@
 @Echo off
 title Spartan Flash Animate Version Checker
 echo GAWN2600 Pro
-echo Version 25H2 (Build 26090.ge_release.250611-0310, 26090.1)
-echo Full buildtag: 26090.1.ge_release.250611-0310
+echo Version 25H2 (Build 26100.ge_release.250613-1521, 26100.1)
+echo Full buildtag: 26100.1.ge_release.250613-1521
 echo:
 echo New Features and Changes
 echo:
-echo The Bottom-Right corner watermark and Timebomb has been removed, since this build is now production-signed.
+echo Changed The old "GoAniWayneNathaniel2600" branding into "Spartan Flash Animate" inside Start.bat
 echo:
 pause
 cls
