ea948ba8393705b94a3f5efd877a22eaf3e2d43a
StarCobaltAnimator2021 <147932170+StarCobaltAnimator2021@users.noreply.github.com>
2025-06-16 03:18:45 +0700

    Finalization

--- Start.bat
+++ Start.bat
@@ -2,12 +2,12 @@
 echo:
 echo GAWN2600 Pro
 echo:
-echo GoAniWayneNathaniel2600 Will now start
+echo Spartan Flash Animate Will now start
 echo:
 echo Closing this Window Will also close the Software but not the CMD window, Please remember to ALWAYS Close 
 echo it properly by saving the videos first. Then Go back to the home page. and use "close node.bat" To close
 echo the CMD Window. Anyways Remember to smile always while Making Videos :) and Have Fun Making Videos...
 echo:
-echo Build 22621.zn_release.240314-1858
+echo Build 26100.ge_release.250613-1521
 echo:
 npm start
