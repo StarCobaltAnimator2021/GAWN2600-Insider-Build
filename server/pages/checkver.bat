ea13e76a8df272965fdc7f4e7dd710f775df10c3
StarCobaltAnimator2021 <147932170+StarCobaltAnimator2021@users.noreply.github.com>
2025-09-16 01:36:14 +0700

    hold up

--- server/pages/checkver.bat
+++ server/pages/checkver.bat
@@ -1,12 +1,21 @@
 @Echo off
 title Spartan Flash Animate Version Checker
 echo GAWN2600 Pro
-echo Version 25H2 (Build 26100.ge_release.250613-1521, 26100.268)
-echo Full buildtag: 26100.268.ge_release_svc_prod1.250615-0754
+echo Version 26H1 (26200.2007)
+echo Full buildtag: 26200.2007.ge_release_alpha.250806-2048
+echo Experience: SFA Feature Experience Pack 1000.26100.93.0
 echo:
 echo New Features and Changes
 echo:
-echo Changed The old "GoAniWayneNathaniel2600" branding into "Spartan Flash Animate" inside Start.bat
+echo 1. Re-Added the Secret DirectX Diagnostic Tool after it was removed in Service Pack 6.
+echo:
+echo 2. An new "Experience" section has been added to checkver.bat
+echo:
+echo 3. The Official Links has been re-added into the "pages" folder after it was temporarily removed.
+echo:
+echo 4. Replaced one of the pictures inside "pages" folder.
+echo:
+echo 5. The UI were reverted back to the ones first seen in build 26058.1000 due to Summer Season is nearing the end.
 echo:
 pause
 cls
