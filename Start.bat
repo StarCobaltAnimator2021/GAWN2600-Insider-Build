16d53e03a699c1ad63ed2bd72e55f944b9e82029
StarCobaltAnimator2021 <147932170+StarCobaltAnimator2021@users.noreply.github.com>
2025-09-18 01:40:05 +0700

    Canary Channel Specific

--- Start.bat
+++ Start.bat
@@ -1,13 +1,6 @@
 @echo off
 echo:
-echo GAWN2600 Pro
-echo:
-echo Spartan Flash Animate Will now start
-echo:
-echo Closing this Window Will also close the Software but not the CMD window, Please remember to ALWAYS Close 
-echo it properly by saving the videos first. Then Go back to the home page. and use "close node.bat" To close
-echo the CMD Window. Anyways Remember to smile always while Making Videos :) and Have Fun Making Videos...
-echo:
-echo Build 26100.ge_release.250613-1521
+echo SFA ND23 Pro Insider Preview
+echo Evaluation copy. Build 26225.ge_prerelease.250917-2140
 echo:
 npm start
--- server/pages/checkver.bat
+++ server/pages/checkver.bat
@@ -1,12 +1,35 @@
 @Echo off
 title Spartan Flash Animate Version Checker
-echo GAWN2600 Pro
-echo Version 25H2 (Build 26100.ge_release.250613-1521, 26100.1)
-echo Full buildtag: 26100.1.ge_release.250613-1521
+echo SFA ND23 Pro Insider Preview
+echo Version 25H2 (26225.5000)
+echo Full buildtag: 26225.5000.ge_prerelease.250917-2140
+echo Experience: SFA Feature Experience Pack 1000.26100.102.0
+echo:
+echo Evaluation copy. Expires 10/31/2025
+echo:
+echo:
+echo For New Features and Changes listed, press any key...
+echo:
+pause
+cls
 echo:
 echo New Features and Changes
 echo:
-echo Changed The old "GoAniWayneNathaniel2600" branding into "Spartan Flash Animate" inside Start.bat
+echo 1. Re-Added the Secret DirectX Diagnostic Tool after it was removed in Service Pack 6.
+echo:
+echo 2. An new "Experience" section has been added to checkver.bat, along with the Timebomb section to indicate which date that specific build expires.
+echo:
+echo 3. The Official Links has been re-added into the "pages" folder after it was temporarily removed.
+echo:
+echo 4. Replaced one of the pictures inside "pages" folder.
+echo:
+echo 5. The UI were reverted back to the ones first seen in build 26058.1000 due to Summer Season is nearing the end.
+echo:
+echo 6. Separate Flash Player Program are no longer required.
+echo:
+echo 7. The "Cartoon Classics" and "Stick Figure" theme has been removed from Theme Selection List Inside "Make a Video" page due to missing icon files.
+echo:
+echo 8. The remaining old GAWN2600 branding has been completely replaced with the new branding, as a result, Spartan Flash Animate in short terms will now be "SFA ND23"
 echo:
 pause
 cls
