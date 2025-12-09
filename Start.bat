9176cbbadeb9c0bd815f93b289c7cb1e1b5165a4
StarCobaltAnimator2021 <147932170+StarCobaltAnimator2021@users.noreply.github.com>
2025-12-10 01:41:15 +0700

    halt

--- Start.bat
+++ Start.bat
@@ -1,6 +1,6 @@
 @echo off
 echo:
 echo SFA ND23 Pro Insider Preview
-echo Evaluation copy. Build 26231.ge_prerelease.250919-1512
+echo Evaluation copy. Build 26236.ge_prerelease.251002-1201
 echo:
 npm start
--- server/pages/checkver.bat
+++ server/pages/checkver.bat
@@ -1,9 +1,9 @@
 @Echo off
 title Spartan Flash Animate Version Checker
 echo SFA ND23 Pro Insider Preview
-echo Version 25H2 (26231.5001)
-echo Full buildtag: 26231.5001.ge_prerelease.250919-1512
-echo Experience: SFA Feature Experience Pack 1000.26100.119.0
+echo Version 25H2 (26236.5000)
+echo Full buildtag: 26236.5000.ge_prerelease.251002-1201
+echo Experience: SFA Feature Experience Pack 1000.26100.122.0
 echo:
 echo Evaluation copy. Expires 8/14/2026
 echo:
@@ -15,18 +15,12 @@ cls
 echo:
 echo New Features and Changes
 echo:
-echo 1. The Timebomb has been extended from October 31, 2025 to August 14, 2026.
-echo:
-echo 2. Updated The Secret DirectX Diagnostic Tool Version. (5.3.2600.5512 -> 10.0.26100.5074)
-echo:
-echo 3. The file "COMPAT_STUB" and "gawn2600.cert", which were removed since Service Pack 6, has been added back.
+echo No new features currently included in this build.
 echo:
 pause
 cls
 echo Bug Fixes.
 echo:
-echo 1. Addressed an issue where the color hovering on between Videos and Starters Tab in Video List page are blue instead of accent matching UI color.
-echo:
-echo 2. Mitigated an issue where the complie date on the footer wasn't matching with the actual full buildtag and actual complie date in build 26225.
+echo No bugfixes currently included in this build.
 echo:
 pause
